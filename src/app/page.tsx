b8c32a2ba4bc5ea58d8c0905056686d9ab24b8c8
Enzo Cerqueira <enzo.cerqueira42@gmail.com>
2025-08-01 17:32:04 -0300

    configuring tailwind css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import clsx from "clsx";
+
 export default function HomePage() {
-  return <h1>Olá de dentro de page.tsx</h1>;
+    return (
+        <h1
+            className={clsx(
+                "text-6xl",
+                "font-bold",
+                "text-blue-500",
+                "hover:text-blue-200",
+                "hover:bg-blue-500",
+                "transition",
+                "duration-500"
+            )}
+        >
+            Texto no meu H1
+        </h1>
+    );
 }
